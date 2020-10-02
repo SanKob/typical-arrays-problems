8290f169a17370b12744e528ffb6d3f25072a00e
San <sanek2184@rambler.ru>
2020-10-02 18:02:53 +0300

    feat: change functions in index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,39 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (array === undefined || array.length === 0) {
+    return 0;
+  }
+  let arr = array;
+  let min = array[0];
+  for (let i=0; i <arr.length; i++) {
+    if (min > arr[i]) {
+      min = arr[i];
+    }
+  }
+  return min;
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (array === undefined || array.length === 0) {
+    return 0;
+  };
+  let arr = array;
+  let max = array[0];
+  for (let i=0; i <arr.length; i++) {
+    if (max < arr[i]) {
+      max = arr[i];
+    }
+  }
+  return max;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (array === undefined || array.length === 0) {
+    return 0;
+  };
+  let sumOfArr = array.reduce((sum, curr) => {
+    return sum + curr;
+  });
+  let avg = sumOfArr / array.length;
+  return avg;
 }
